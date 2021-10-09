924c00de2ec4f31ec7ba580082a73609129ccfb3
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-10-09 14:18:12 +0100

    navbaradded

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
+import AllCountries from "./All Countries";
 import Home from "./Home";
-=======
-
-import Home from "./Home";
-
->>>>>>> 0070704c86ceb9c3454fbc406be95b47cb18afe9
+import Navbar from "./Navbar";
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/Countries" component={AllCountries} />
     </Switch>
   </BrowserRouter>
 );
