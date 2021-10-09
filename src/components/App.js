8f2cd51504566c26c92c678eb946df1e72284fcd
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-10-09 11:02:34 +0100

    initalnavbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => (
   <BrowserRouter>
@@ -7,6 +7,6 @@ const App = () => (
       <Route exact path="/" component={() => <p>Hello</p>} />
     </Switch>
   </BrowserRouter>
-)
+);
 
-export default App
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar">
+        <div className="container">
+          <div className="navbaritems">
+            <Link to="/">Home</Link>
+            <Link to="/countries">Countries</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
