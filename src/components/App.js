1a892e81f04127723082af6624cf74eed4040380
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-09 13:47:38 +0100

    almost complete auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
+import Register from '../components/auth/Register.js'
+import Login from "./auth/Login";
 
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
 );
--- src/lib/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const baseUrl = 'http://localhost:8001'
-
-export const registerUser = (formData) => {
-    return axios.post(`${baseUrl}/register`, formData)
-}
