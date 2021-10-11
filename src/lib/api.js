6cb4b4ba1f459819be8005631806f79f6754fb53
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-11 23:25:05 +0100

    members

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,15 @@
-
-import axios from "axios";
+import axios from 'axios'
 
 // All methods for interacting with the api
 
 export const getAllCountries = async () => {
-  const data = await axios.get(`http://localhost:8001/api/countries`);
-  console.log("this is data returning from the API", data);
+  const data = await axios.get(`http://localhost:8001/api/countries`)
+  // console.log('this is data returning from the API', data)
+  return data
+}
 
-  return data;
-};
+export const createCountry = async (formData) => {
+  const data = await axios.post(`http://localhost:8001/api/countries`)
+  console.log(`👀 This is the response form the CREATECOUNTRY api call`, data)
+  return data
+}
