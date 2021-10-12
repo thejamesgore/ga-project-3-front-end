9fe78cb18c6a4ae2a547885080cc0bec0bdc34a2
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-12 01:46:38 +0100

    now getting user ID back from api

--- src/lib/api.js
+++ src/lib/api.js
@@ -10,6 +10,12 @@ export const getAllCountries = async () => {
 
 export const createCountry = async (formData) => {
   const data = await axios.post(`http://localhost:8001/api/countries`)
-  console.log(`👀 This is the response form the CREATECOUNTRY api call`, data)
+  // console.log(`👀 This is the response form the CREATECOUNTRY api call`, data)
   return data
+}
+
+export const getUser = async (id) => {
+  const user = await axios.get(`http://localhost:8001/api/user/${id}`)
+  // console.log(`This is the user data from getUser!`, user)
+  return user
 }
--- src/pages/MembersDashboard.js
+++ src/pages/MembersDashboard.js
@@ -1,12 +1,28 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 import CreateCountryForm from '../components/CreateCountryForm'
+import { getAllCountries, getUser } from '../lib/api'
 
 export default function MembersHome() {
   // function that automatically gets all countries for user based on id
   // function that sorts through that data and only adds countries to an array
   // Displays every country travelled to underneath map
   // functinon that passes each location from the array into map function and drops pin onto map
+  const [userId, setUserId] = useState()
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    getAllCountries().then(function (response) {
+      setCountries(response.data)
+    })
+  }, [])
+
+  console.log(countries)
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    getUser(token).then((response) => setUserId(response))
+  }, [])
+  console.log('User ID FROM API IS >> ', userId?.data._id)
 
   return (
     <div className="members-home">
@@ -25,7 +41,7 @@ export default function MembersHome() {
 
         <div className="right-side">
           <h1>Create New Trip</h1>
-          <CreateCountryForm />
+          {/* <CreateCountryForm /> */}
         </div>
       </div>
       <div className="visted-bar">
