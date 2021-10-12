6ef758574ee7026f21807cfe8351b0a6238a1111
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2021-10-12 21:00:00 +0100

    updated bugs for searches

--- src/lib/api.js
+++ src/lib/api.js
@@ -11,18 +11,9 @@ export const getAllCountries = async () => {
 }
 
 // getAllCountriesById
-const getAllCountriesById = (async) => {
-  const data = await axios.get('localhost:3000/api/countries/:id')
+export const getAllCountriesById = async () => {
+  const data = await axios.get(`http://localhost:8001/api/countries/${id}`)
   console.log('This is our countries returning with Ids')
-}
-axios(getAllCountriesById)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data))
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
-  // console.log('this is data returning from the API', data)
   return data
 }
 
