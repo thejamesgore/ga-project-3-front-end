6d2ee0fd03aafca929bf90075565ea8e214dcaff
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-10-12 20:15:49 +0100

    start of search bar

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,23 @@
-
-import axios from "axios";
+import axios from 'axios'
 
 // All methods for interacting with the api
 
 export const getAllCountries = async () => {
-  const data = await axios.get(`http://localhost:8001/api/countries`);
-  console.log("this is data returning from the API", data);
+  const data = await axios.get(`http://localhost:8001/api/countries`)
+  console.log('this is data returning from the API', data)
 
-  return data;
-};
+  return data
+}
+
+// getAllCountriesById
+const getAllCountriesById = (async) => {
+  const data = await axios.get('localhost:3000/api/countries/:id')
+  console.log('This is our countries returning with Ids')
+}
+axios(getAllCountriesById)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data))
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
