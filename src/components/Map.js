03f91411b83ac782e97e134e80311653be5fcca6
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-12 05:36:03 +0100

    mapped coordinates over map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { MapContext } from 'react-map-gl'
 import { getCoordinates } from '../lib/helpers'
 
 export default function Map() {
@@ -8,8 +8,23 @@ export default function Map() {
 
   useEffect(() => {
     console.log(`I'm logging form the map function`)
-    getCoordinates('london')
+    getCoordinates('ireland')
   }, [])
+
+  const coordinates = [
+    {
+      lat: 51.5073509,
+      lng: -0.1277583,
+    },
+    {
+      lat: 48.856614,
+      lng: 2.3522219,
+    },
+    {
+      lat: 53.1423672,
+      lng: -7.692053599999999,
+    },
+  ]
 
   const [viewport, setViewport] = useState({
     width: 500,
@@ -26,7 +41,35 @@ export default function Map() {
         // mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         mapboxApiAccessToken="pk.eyJ1IjoiamFtZXNnb3JlIiwiYSI6ImNrdW5qMWZnYTFucGgycG82MGVwaW83aXkifQ.ClJikErzkit5U1DNhCCwpw"
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      />
+      >
+        {/* MAP OVER THE MARKERS FUNCTION WITH CO-ORDINATES */}
+        {coordinates?.map((coordinates) => (
+          <CustomMarker lat={coordinates.lat} lng={coordinates.lng} />
+        ))}
+
+        {/* END OF MAP FUNCTION */}
+      </ReactMapGL>
+    </div>
+  )
+}
+
+function CustomMarker(props) {
+  const context = React.useContext(MapContext)
+
+  const { lng, lat } = props
+
+  const [x, y] = context.viewport.project([lng, lat])
+
+  const markerStyle = {
+    position: 'absolute',
+    background: '#fff',
+    left: x,
+    top: y,
+  }
+
+  return (
+    <div style={markerStyle}>
+      ({lng}, {lat})
     </div>
   )
 }
