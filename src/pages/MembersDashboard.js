1948d1e9532912cb092d71e6fc7b1b6c1dd71f75
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-10-12 03:04:11 +0100

    user countries now display in members area

--- src/pages/MembersDashboard.js
+++ src/pages/MembersDashboard.js
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import CreateCountryForm from '../components/CreateCountryForm'
+import VisistedPlaces from '../components/VisistedPlaces'
 import { getAllCountries, getUser } from '../lib/api'
 
 export default function MembersHome() {
-  // function that automatically gets all countries for user based on id
   // function that sorts through that data and only adds countries to an array
   // Displays every country travelled to underneath map
   // functinon that passes each location from the array into map function and drops pin onto map
-  const [userId, setUserId] = useState()
+  const [userId, setUserId] = useState('')
   const [countries, setCountries] = useState([])
+  const [userCountries, setUserCountries] = useState()
 
   useEffect(() => {
     getAllCountries().then(function (response) {
       setCountries(response.data)
     })
   }, [])
-
-  console.log(countries)
+  // console.log(countries)
 
   useEffect(() => {
     const token = localStorage.getItem('token')
     getUser(token).then((response) => setUserId(response))
   }, [])
-  console.log('User ID FROM API IS >> ', userId?.data._id)
+  // console.log('User ID FROM API IS >> ', userId?.data._id)
+
+  useEffect(() => {
+    // console.log(`The user id is >>> `, userId)
+    let visitedCountries = countries.filter(
+      (country) => country.createdBy === userId.data._id
+    )
+
+    visitedCountries.map(({ name }) => {
+      console.log(`Visisted countries are >>> `, { name })
+    })
+    // console.log(`PARSED VISITED COUNTRIES >>> `, parsedVisited)
+    setUserCountries(visitedCountries)
+  }, [userId])
+  // console.log('countries created by logged in user  >>>', userCountries)
 
   return (
     <div className="members-home">
@@ -45,6 +59,11 @@ export default function MembersHome() {
         </div>
       </div>
       <div className="visted-bar">
+        <div>
+          {userCountries?.map(({ name }) => (
+            <VisistedPlaces key={name} name={name} />
+          ))}
+        </div>
         <h1>THIS IS WHERE THE TOTAL COUNTRIES VISITED BAR GOES</h1>
       </div>
     </div>
