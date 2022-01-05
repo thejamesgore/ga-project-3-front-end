0b4c21c96855fd0ca3f1bbc8946f277867bd53a7
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-01-05 17:45:27 +0000

    added cors anywhere

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 const env =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? new webpack.EnvironmentPlugin({ ...process.env })
-    : new Dotenv();
+    : new Dotenv()
 
 module.exports = (webpackEnv) => {
   const publicPath =
-    webpackEnv.NODE_ENV === "local"
+    webpackEnv.NODE_ENV === 'local'
       ? {
-          publicPath: "/",
+          publicPath: '/',
         }
-      : {};
+      : {}
   return {
-    entry: "./src/index.js",
+    entry: './src/index.js',
     output: {
-      filename: "bundle.js",
-      path: path.resolve("./build"),
+      filename: 'bundle.js',
+      path: path.resolve('./build'),
       ...publicPath,
     },
     module: {
       rules: [
-        { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
-        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+        { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
         {
           test: /\.s(a|c)ss$/,
-          use: ["style-loader", "css-loader", "sass-loader"],
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
-        { test: /\.(png|jpe?g|gif)$/i, use: "file-loader" },
+        { test: /\.(png|jpe?g|gif)$/i, use: 'file-loader' },
       ],
     },
     devServer: {
@@ -37,14 +37,14 @@ module.exports = (webpackEnv) => {
       // contentBase: path.resolve('src'),
       hot: true,
       open: true,
-      port: 8001,
+      port: 8002,
       // watchContentBase: true,
       historyApiFallback: true,
       proxy: {
         // ! This tells webpack about express. Any requests to we prefix with /api will get redirected to express!
-        "/api": {
+        '/api': {
           // Our express server. It's not running on https, so it'll be http!
-          target: "http://localhost:3000",
+          target: 'http://localhost:3000',
           // This is for development purposes only, so we aren't too worried about securing our proxy!
           secure: false,
         },
@@ -54,11 +54,11 @@ module.exports = (webpackEnv) => {
       new Dotenv(),
       new webpack.HotModuleReplacementPlugin(),
       new HtmlWebpackPlugin({
-        template: "src/index.html",
-        filename: "index.html",
-        inject: "body",
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body',
       }),
       env,
     ],
-  };
-};
+  }
+}
